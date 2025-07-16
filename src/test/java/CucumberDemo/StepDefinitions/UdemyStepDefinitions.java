33bfb3ea35d0cc1ba232945a2a565588a1056be3
Reimann <mhuseyinbayramoglu@gmail.com>
2025-07-16 22:10:27 +0300

    16/07/2025

---
+++ src/test/java/CucumberDemo/StepDefinitions/UdemyStepDefinitions.java
@@ -0,0 +1,34 @@
+package CucumberDemo.StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UdemyStepDefinitions {
+
+	
+	@Given("Udemy Web sitesine git")
+	public void udemy_web_sitesine_git() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Arama kutucuğuna tikla")
+	public void arama_kutucuğuna_tikla() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Yazilim yaz ve ara")
+	public void yazilim_yaz_ve_ara() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Yazilim Kurslarinin geldigini kontrol et")
+	public void yazilim_kurslarinin_geldigini_kontrol_et() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+}
---
+++ target/test-classes/CucumberDemo/Features/Udemy.feature
@@ -0,0 +1,11 @@
+Feature: Udemy
+
+	Scenario: Udemy Sitesine git ve arama yap
+	Given Udemy Web sitesine git
+	When Arama kutucuğuna tikla
+	When Yazilim yaz ve ara
+	Then Yazilim Kurslarinin geldigini kontrol et
+	
+	
+	
+	
